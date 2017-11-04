26313e15907661f7174e5c52d358ddcb67f02b20
Kenny Brawner <kjbrawner22@gmail.com>
2017-11-03 23:00:28 -0400

    added donor and donee routes

--- app/app.py
+++ app/app.py
@@ -8,5 +8,15 @@ def index():
   title = "MedSend"
   return render_template('index.html', title=title)
 
+@app.route('/donor', methods= ["POST"])
+def donor():
+	title = "MedSend"
+	user = {"firstName" : "Kenny", "lastName" : "Brawner"}
+	return render_template('donor.html', user=user, title=title)
+
+@app.route('/donee')
+def donee():
+	return render_template('donee.html')
+
 if __name__=='__main__':
   app.run(debug=True)
